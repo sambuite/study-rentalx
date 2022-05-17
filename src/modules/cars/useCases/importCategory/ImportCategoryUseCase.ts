c695fd7a21747cbb9b054cef87a32b5f3e2daf3e
sambuite <murilosambuite@gmail.com>
2022-05-16 23:48:57 -0300

    add ImportCategory use case; add upload with multer

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,57 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+export class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  readCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.readCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const categoryExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryExists) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+  }
+}
